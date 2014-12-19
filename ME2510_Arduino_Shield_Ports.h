e745e422dcd1cefd39159cce73b543667dd34357
MeKyleH <kyle.a.holt@gmail.com>
2014-12-19 17:15:51 -0500

    Create ME2510_Arduino_Shield_Ports.h

---
+++ ME2510_Arduino_Shield_Ports.h
@@ -0,0 +1,44 @@
+#pragma once
+
+//======================================================
+// Updated Port Definitions for the Rev 2.0 Shield Board
+//======================================================
+
+const int MOTOR_0_EN_PORT = 11;
+const int MOTOR_0_A_PORT = 12;
+const int MOTOR_0_B_PORT = 13;
+
+const int MOTOR_1_EN_PORT = 3;
+const int MOTOR_1_A_PORT = 7;
+const int MOTOR_1_B_PORT = 8;
+
+const int START_BUTTON_PORT = 4;
+const int STOP_BUTTON_PORT = 2;
+
+const int SERVO_SIGNAL_PORT = 9;
+
+const int PHOTO_0_PORT = 0;
+const int PHOTO_1_PORT = 1;
+const int BATTERY_V_PORT = 4;
+const int POTENTIOMETER_PORT = 5;
+
+const int RX_PORT = 0;
+const int TX_PORT = 1;
+
+const int LOAD_0_PORT = 5;
+const int LOAD_1_PORT = 6;
+
+const int ANALOG_2_PORT = 2;
+const int ANALOG_3_PORT = 3;
+
+const int IENCODER_A_PORT = 2;
+const int IENCODER_B_PORT = 10;
+const int IENCODER_Z_PORT = 6;
+
+const int ENCODER_A_PORT = 7;
+const int ENCODER_B_PORT = 8;
+
+const int EXTERNAL_SWITCH_PORT = 10;
+
+
+
